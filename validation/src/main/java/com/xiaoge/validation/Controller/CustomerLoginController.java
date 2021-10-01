7079b3d61896d96a7aff82c595a042b4b85a0dc1
xiaoyan ge <cgxy1995@gmail.com>
2021-09-30 23:58:46 -0400

    validation part broke

--- validation/src/main/java/com/xiaoge/validation/Controller/CustomerLoginController.java
+++ validation/src/main/java/com/xiaoge/validation/Controller/CustomerLoginController.java
@@ -6,24 +6,35 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.validation.Valid;
 
 @Controller
-public class CustomerLoginController {
+public class CustomerLoginController implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/result").setViewName("result");
+    }
 
     @GetMapping("/")
-    public String showCustomerForm(Customer customer, Model model){
-        model.addAttribute("customer", customer);
-        System.out.println("in get");
+    public String showMain(Customer customer){
         return "customer-form";
     }
+//    @GetMapping("/customer-form")
+//    public String showCustomerForm( Model model){
+//        model.addAttribute("customer", new Customer());
+//        System.out.println("in get");
+//        return "customer-form";
+//    }
     @PostMapping("/")
-    public String checkPersonInfo(@Valid Customer customer, BindingResult bindingResult){
+    public String checkPersonInfo(@Valid Customer customer, Model model, BindingResult bindingResult){
         if(bindingResult.hasErrors()){
-            System.out.println("in post");
             return"customer-form";
         }
+        model.addAttribute(customer);
+
         return "redirect:/result";
     }
 }
