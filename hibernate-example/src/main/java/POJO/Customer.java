650af21c326cbdcaae26c52a5332846b95d85cc8
xiaoyan ge <cgxy1995@gmail.com>
2021-10-04 20:05:25 -0400

    add POJO and customer

---
+++ hibernate-example/src/main/java/POJO/Customer.java
@@ -0,0 +1,14 @@
+package POJO;
+
+import lombok.Data;
+
+@Data
+public class Customer {
+    //TODO: add validation
+
+    private String id;
+    private String firstName;
+    private String lastName;
+    private int age;
+
+}
